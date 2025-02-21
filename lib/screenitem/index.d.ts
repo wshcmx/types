7164b8f66f84b375fbc8d0fa38ca5ff179881dfd
SERROC <kido-exe@yandex.ru>
2025-02-21 10:35:35 +0300

    Adjusted ScreenItem type

--- lib/screenitem/index.d.ts
+++ lib/screenitem/index.d.ts
@@ -1,5 +1,28 @@
 type ScreenItem = {
+  ActiveElem: ScreenItem;
+  Env: XmlElem<unknown>;
+  HasSetValueAction: boolean;
+  ImageUrl: string;
+  IsEnabled: boolean;
+  LoopArray: unknown[];
+  Name: string;
+  Parent: ScreenItem | typeof Screen;
+  Ps: XmElem<unknown>;
+  Screen: typeof Screen;
+  Source: unknown;
+  SourceDesc: string;
+  Title: string;
+  Type: string;
 
+  /**
+   * Вызывает изменение значение источника (SOURCE) у элемента.
+   * @param value - Новое заначение источника
+   * @example
+   * ```
+   * GetSampleItem().SetSourceValue(newValue);
+   * ```
+   */
+  SetSourceValue(value: unknown): void;
 };
 
 /* eslint-disable @typescript-eslint/no-redeclare */
