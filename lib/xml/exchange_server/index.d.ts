8bd3c68ecf25d57b8a8b7a0bb46e52a510c9b126
umbrik <693254+umbrik@users.noreply.github.com>
2025-02-25 16:46:49 +0100

    Revert back exchange_server to 10.0.0

--- lib/xml/exchange_server/index.d.ts
+++ lib/xml/exchange_server/index.d.ts
@@ -1,63 +1,86 @@
-interface ExchangeBaseDocumentExchangeObjectFilter extends ViewConditionsBase {
+interface ExchangeBaseExchangeObjectFilter extends ViewConditionsBase {
   scheme_id: XmlElem<string | null, typeof lists.view_conditions_schemes>;
 }
 
-interface ExchangeBaseDocumentExchangeObject {
+interface ExchangeBaseExchangeObject {
   name: XmlElem<string | null, typeof common.exchange_object_types>;
-  filter: XmlElem<ExchangeBaseDocumentExchangeObjectFilter | null>;
-  /** @default false */
+  filter: XmlElem<ExchangeBaseExchangeObjectFilter | null>;
   is_invariable: XmlElem<boolean>;
   xquery_qual: XmlElem<string | null>;
 }
 
-type ExchangeBaseDocumentTopElem = XmlTopElem & {
-  Doc: ExchangeBaseDocument;
-  /** @default smtp */
+interface ExchangeBase {
   exchange_protocol: XmlElem<string>;
   http_server_url: XmlElem<string | null>;
   smtp_mail_address: XmlElem<string | null>;
   smtp_server_url: XmlElem<string | null>;
-  /** @default false */
   use_smtp_authenticate: XmlElem<boolean>;
   smtp_login: XmlElem<string | null>;
   smtp_password: XmlElem<string | null>;
-  /** @default last_exchange */
   exchange_type: XmlElem<string, typeof common.upload_types>;
-  /** @default false */
   only_create: XmlElem<boolean>;
   lately_exchange_date: XmlElem<Date | null>;
-  /** @default 30 */
   lately_exchange_period: XmlElem<number | null>;
-  /** Лимит количества записей в одном пакете */
   limit_count: XmlElem<number | null>;
-  /** @default true */
   allow_enlarge_period: XmlElem<boolean>;
-  /** @default all */
   exchange_objects_type: XmlElem<string>;
-  exchange_objects: XmlMultiElem<ExchangeBaseDocumentExchangeObject | null>;
-  /** @default true */
+  exchange_objects: XmlMultiElem<ExchangeBaseExchangeObject | null>;
   use_basic_authenticate: XmlElem<boolean>;
-  /** @default true */
   is_wt_to_wt: XmlElem<boolean>;
-  /** @default false */
   process_deleted_objects: XmlElem<boolean>;
-  /** @default false */
   process_custom_templates: XmlElem<boolean>;
-  /** @default false */
   process_lists: XmlElem<boolean>;
-  /** @default false */
   process_access_roles: XmlElem<boolean>;
-  /** @default all */
   exchange_object_select_type: XmlElem<string>;
   filter_text: XmlElem<string | null>;
-  /** @default false */
   is_invariable: XmlElem<boolean>;
   process_eval: XmlElem<string | null>;
+}
+
+interface ExchangeServerDocumentDownload extends ExchangeBase {
+  /** Не обновлять объекты с большей датой модификации */
+  take_dest_modification_date: XmlElem<boolean>;
+}
+
+interface ExchangeServerDocumentUpload extends ExchangeBase {
+  clear_resource_data: XmlElem<boolean>;
+}
+
+type ExchangeServerDocumentTopElem = XmlTopElem &
+CustomElemsBase & {
+  Doc: ExchangeServerDocument;
+  /** Код */
+  code: XmlElem<string | null>;
+  /** Название */
+  name: XmlElem<string | null>;
+  /** Пароль сервера */
+  server_password: XmlElem<string | null>;
+  /** Зарегистрирован */
+  is_active: XmlElem<boolean>;
+  /** Настройки получения */
+  download: XmlElem<ExchangeServerDocumentDownload | null>;
+  /** Обновлять данные в базе при совпадении ID */
+  update_exist_package_obj: XmlElem<boolean>;
+  /** Автоматически публиковать тест */
+  auto_publish_test: XmlElem<boolean>;
+  /** Настройки отправки */
+  upload: XmlElem<ExchangeServerDocumentUpload | null>;
+  /** Дата последней отправки данных */
+  last_upload_date: XmlElem<Date | null>;
+  /** Дата последнего приема данных */
+  last_download_date: XmlElem<Date | null>;
+  /** Версия сервера при приеме данных */
+  server_version: XmlElem<string | null>;
+  /** Прием данных */
+  allow_receive_data: XmlElem<boolean>;
+  /** Информация об объекте */
+  doc_info: XmlElem<DocInfoBase | null>;
+  /** Категория */
+  role_id: XmlMultiElemObject<number | null>;
 };
 
-type ExchangeBaseDocument = XmlDocument & {
-  TopElem: ExchangeBaseDocumentTopElem;
-  exchange_base: ExchangeBaseDocumentTopElem;
-  exchange_server: XmlElem<unknown | null>;
+type ExchangeServerDocument = XmlDocument & {
+  TopElem: ExchangeServerDocumentTopElem;
+  exchange_server: ExchangeServerDocumentTopElem;
   DocDesc(): string;
 };
