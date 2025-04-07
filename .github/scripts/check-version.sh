beca49d03a9582862f43881c50d0cf8e1b4a6319
umbrik <693254+umbrik@users.noreply.github.com>
2025-04-07 22:46:51 +0200

    Add auto update local version (#124)
    * Add auto update local version
    
    * Update version

--- .github/scripts/check-version.sh
+++ .github/scripts/check-version.sh
@@ -1,3 +1,17 @@
-localVersion=$(cat package.json | jq -r '.version')
+localVersion=$(jq -r '.version' < package.json)
 masterVersion=$(git show origin/main:package.json | jq -r '.version')
-[ "${localVersion}" = "`echo -e "${localVersion}\n${masterVersion}" | sort -V | head -n1`" ] && echo '::error::Package version should be upgraded' && exit 1 || exit 0
+
+if [ "${localVersion}" = "$(echo -e "${localVersion}\n${masterVersion}" | sort -V | head -n1)" ]; then
+    nextVersion=$(echo "${masterVersion}" | awk -F. '{printf "%d.%d.%d", $1, $2, $3+1}')
+
+    if [ -z "${GITHUB_ACTIONS}" ]; then
+        git remote update
+        jq ".version = \"${nextVersion}\"" package.json > package.tmp
+        mv package.tmp package.json
+        npm install
+        git add package*.json
+    else
+        echo "Package version should be upgraded. Current version: ${localVersion}, minimum required version: ${nextVersion}"
+        exit 1
+    fi
+fi
